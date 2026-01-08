be04a301311b1cc022b5613da6c192864b916af8
David <115666355+David-237-9@users.noreply.github.com>
2026-01-08 21:20:40 +0000

    Minor Changes

--- GPSApp/app/src/main/java/dev/david2379/gpsapp/logic/gps/GpsActivity.kt
+++ GPSApp/app/src/main/java/dev/david2379/gpsapp/logic/gps/GpsActivity.kt
@@ -15,8 +15,6 @@ import dev.david2379.gpsapp.ui.gps.GpsScreen
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-const val GPS_REFRESH_RATE_MS = 500L
-
 class GpsActivity : ComponentActivity() {
     private var refreshGpsJob: Job? = null
 
@@ -39,13 +37,11 @@ class GpsActivity : ComponentActivity() {
                 refreshGpsJob = lifecycleScope.launch {
                     gpsData.startLocationUpdates { newLocation ->
                         if (newLocation != null) {
-                            println("NEW LOCATION #${++counter} ${System.currentTimeMillis()}")
                             gpsLocation = newLocation
                         }
                     }
                 }
             }
-            println("STARTING GPS REFRESH " + System.currentTimeMillis())
             startRefreshJob()
 
             GPSAppTheme {
